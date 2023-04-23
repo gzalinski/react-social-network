1c179a94dd71bea250ca5da98993b4a6f4f9fa67
gzalinschi <gheorghe@enspiredev.com>
2023-04-23 05:17:44 +0300

    fix NavLink Active & Refactor Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,39 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    return (
+        <div className={s.dialog}>
+            <NavLink to={'/dialogs/'+props.id} className={s.dialog}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return (
+        <div className={s.message}>
+            {props.text}
+        </div>
+    );
+}
 
 const Dialogs = (props) => {
     return (
-        <div className={s.item}>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogs__items}>
+                <DialogItem id="1" name="Dimych"/>
+                <DialogItem id="2" name="Valera"/>
+                <DialogItem id="3" name="Andrey"/>
+                <DialogItem id="4" name="Sveta"/>
+                <DialogItem id="5" name="Sasha"/>
+            </div>
+            <div className={s.messages}>
+                <Message text="Hi" />
+                <Message text="How are you ?" />
+                <Message text=":)" />
+            </div>
+        </div>
     );
 }
 
