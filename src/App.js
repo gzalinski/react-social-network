00b096569f8e93eddb0b0889b348384f329240f8
gzalinschi <gheorghe@enspiredev.com>
2023-04-24 17:00:54 +0300

    move bll data to index.js

--- src/App.js
+++ src/App.js
@@ -7,25 +7,6 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const App = (props) => {
 
-    let dataPosts = [
-        { id: 1, title: "title 1", description: "description 1"},
-        { id: 2, title: "title 2", description: "description 2"},
-        { id: 3, title: "title 3", description: "description 3"},
-    ]
-
-    const dataDialogs = [
-        {"id": 1, "name": "Dimych"},
-        {"id": 2, "name": "Andrey"},
-        {"id": 3, "name": "Sveta"},
-        {"id": 4, "name": "Sasha"},
-        {"id": 5, "name": "Viktor"}
-    ]
-
-    const dataMessages = [
-        {"id": 1, "text": "Hi!"},
-        {"id": 2, "text": "How are you ?"},
-        {"id": 3, "text": ":)"}
-    ]
 
     return (
         <BrowserRouter>
@@ -37,8 +18,8 @@ const App = (props) => {
                     </aside>
                     <div className="site__content">
                         <Routes>
-                            <Route path='/profile' element={<Profile posts={dataPosts}/>}/>
-                            <Route path='/dialogs' element={<Dialogs dialogs={dataDialogs} messages={dataMessages}/>}/>
+                            <Route path='/profile' element={<Profile posts={props.posts}/>}/>
+                            <Route path='/dialogs' element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
                         </Routes>
                     </div>
                 </main>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+let dataPosts = [
+    { id: 1, title: "title 1", description: "description 1"},
+    { id: 2, title: "title 2", description: "description 2"},
+    { id: 3, title: "title 3", description: "description 3"},
+]
+
+const dataDialogs = [
+    {"id": 1, "name": "Dimych"},
+    {"id": 2, "name": "Andrey"},
+    {"id": 3, "name": "Sveta"},
+    {"id": 4, "name": "Sasha"},
+    {"id": 5, "name": "Viktor"}
+]
+
+const dataMessages = [
+    {"id": 1, "text": "Hi!"},
+    {"id": 2, "text": "How are you ?"},
+    {"id": 3, "text": ":)"}
+]
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App posts={dataPosts} dialogs={dataDialogs} messages={dataMessages} />
   </React.StrictMode>
 );
 
